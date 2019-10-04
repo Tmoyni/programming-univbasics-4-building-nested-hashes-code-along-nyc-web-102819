8e414b529f1d69e69152cd51b4ded8a20b14409d
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-04 18:31:54 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,19 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {:patriarch => {
-      :name => "Lord Montague",
-      :age => "53"
-  },
-    :matriarch => {
-      :name => "Lady Montague",
-      :age => "54"
-  },
-    :hero => {
-      :name => "Romeo",
-      :age => "15"}
-     :capulet => {:patriarch => {
-      :name => "Lord Capulet",
-      :age => "50"
+     :montague
     },
     :matriarch => {
       :name => "Lady Capulet",
