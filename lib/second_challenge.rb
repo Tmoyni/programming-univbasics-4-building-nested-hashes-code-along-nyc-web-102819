93ee1f399f1df43cd1492c49cf5ec2a7fa44df6a
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-04 18:35:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch {}
-      :matriarch {}
-      :hero {}
-      :hery_friends {}
+      :patriarch => {}
+      :matriarch => {}
+      :hero => {}
+      :hery_friends => {}
       }, 
     :capulet => {
-      :patriarch {}
-      :matriarch {}
-      :heroine {}
-      :heroine_friends {}
+      :patriarch => {}
+      :matriarch => {}
+      :heroine => {}
+      :heroine_friends => {}
       }}
 
   
